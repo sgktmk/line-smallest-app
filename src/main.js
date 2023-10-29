e051b57a0dd97864beeca31973367e629b95207f
sgktmk <sgktmk@gmail.com>
2023-10-29 15:07:45 +0900

    add my liff-app id

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from './App.vue'
 import liff from '@line/liff';
 
 liff.init({
-    liffId: '1234567890-AbcdEfgh', // Use own liffId
+    liffId: '2001258142-JNl6a7XL',
 });
 
 createApp(App).mount('#app')
